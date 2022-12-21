833686f7acf3c69ed5b0f208abfb523ca43e6104
Topmark1 <topsiology11@gmail.com>
2022-12-21 21:09:48 +0100

    frontend project continuation

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,5 +1,6 @@
-
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons';
+//import FontAwesomeIcon from '@fortawesome/free-brands-svg-icons';
 function Body() {
 
     return (
@@ -11,7 +12,7 @@ function Body() {
           <p className='mb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br></br>Unde molestiae esse aspernatur accusantium.</p>
         </div>
         <div className="flex items-center justify-between">
-        <h3 className="text-2xl my-4"></h3> 
+        
         <div className='w-1/3 overflow-hidden p-3 hover:scale-105 transition-transform duration-200 ease-in-out'>
             <img className='rounded cursor-pointer' src="./images/w.jpg" alt="nice house" />
             <div className="bg-white pt-1 px-2 pb-2 rounded" >
@@ -45,9 +46,10 @@ function Body() {
             <p className='mb-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos repudiandae optio non expedita consequuntur perspiciatis aut, aliquam amet odio saepe minus.</p>
             <p className="mb-2">soluta eos corrupti, hic placeat perspiciatis laudantium, corporis nobis ad ipsum?</p>
             <h3 className="text-2xl">Lanre Bayode</h3>  
-            <p className="mb-2">A Happy Client</p>
+            <p className="mb-5">A Happy Client</p>
             <div className="flex">
-              <div className="rounded-full bg-red-500 p-5 items-center content-center">ff</div>
+              <div className="text-2xl cursor-pointer bg-red-300 hidden:hover:bg-red-700 py-2 px-3 rounded-full shadow text-white mr-4 flex items-center"><FontAwesomeIcon icon={faAngleLeft} /></div>
+              <div className="text-2xl cursor-pointer bg-red-600 hover:bg-red-700 py-2 px-3 rounded-full shadow text-white mr-4 flex items-center"><FontAwesomeIcon  icon={faAngleRight} /></div>
             </div>
           </div>
         </div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,68 @@
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faFacebookF,faTwitter,faGithub} from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
-      <div className="footer">
-         footer
+    <div className="p-12 footer">
+             {/*Subcribe*/}
+      <div className='flex items-center justify-between home-foot mb-10'>
+        
+        <div className='w-1/2 text-4xl'>Subcribes to Our Newsletter</div>
+        <div className="w-1/2">
+        <form action="">
+        <div className='w-80 flex rounded border-solid border-2 border-slate-200 p-2 items-center'>
+        <FontAwesomeIcon className='hover:opacity-0' icon={faEnvelope} />
+          <input className='text-xl pl-4' type='email' name='email' placeholder='your email address'></input>
+          <button className='text-xl cursor-pointer bg-red-600 hover:bg-red-700 py-1.5 px-5 rounded shadow text-white' type='submit'>Subcribe</button>
+        </div>
+        </form>
+        </div>
       </div>
+      {/*footer*/}
+      <hr />
+      <div className="flex justify-between mt-2 mb-5">
+        <div className='w-2/5 pr-8'>
+        <h3 className="text-3xl">DesignPlus.</h3>
+        <p className="text-xl font-thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, impedit!</p>
+        </div>
+        <div className="w-1/5">
+          <p className='mb-4'>Other Page</p>
+          <p className='font-thin'>Home</p>
+          <p className='font-thin'>About Us</p>
+          <p className='font-thin'>Services</p>
+          <p className='font-thin'>Contact us</p>
+        </div>
+        <div className="w-1/5">
+        <p className='mb-4'>Quick Link</p>
+          <p className='font-thin'>Privacy Policy</p>
+          <p className='font-thin'>Term of services</p>
+          <p className='font-thin'>Branches</p>
+          <p className='font-thin'>Credits</p>
+        </div>
+        <div className="w-1/5">
+        <p className='mb-4'>Contact Us</p>
+          <p className='font-thin'>topsiology11@gmail.com</p>
+          <p className='font-thin'>+2348086322372</p>
+          <p className='font-thin'>6666578</p>
+        </div>
+      </div>
+      <hr />
+      <div className='flex justify-between mt-2'>
+      <div className="w-1/2 text-xl">Copyright@2023 Topmarktech x Hademathtech</div>
+      <div className='w-1/2 flex pl-64 space-x-50'>
+      <div className="text-2xl cursor-pointer border-2 bg-white hover:bg-slate-300 py-2 px-3 rounded-full shadow text-slate-900 mr-4 flex items-center">
+        <FontAwesomeIcon  icon={faFacebookF} />
+      </div>
+      <div className="text-2xl cursor-pointer border-2 bg-white hover:bg-slate-300 py-2 px-3 rounded-full shadow text-slate-900 mr-4 flex items-center">
+        <FontAwesomeIcon  icon={faTwitter} />
+      </div>
+      <div className="text-2xl cursor-pointer border-2 bg-white hover:bg-slate-300 py-2 px-3 rounded-full shadow text-slate-900 mr-4 flex items-center">
+        <FontAwesomeIcon  icon={faGithub} />
+      </div>
+      </div>
+      </div>
+    </div>
     )
   }
   
